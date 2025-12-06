2a0b470115622d031966af4e87ac17565cabeb65
Mariano <mariano12martinez@gmail.com>
2025-12-06 17:03:14 -0400

    Finish first proyect

--- proyect_vue01/app.js
+++ proyect_vue01/app.js
@@ -2,9 +2,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            task: 'Ir a la playa',
+            taskA: 'Ir a la playa',
+            taskB: 'Ir a la montaña',
+            vueLink: 'https://vuejs.org/',
         };
     },
+    methods: {
+        changeTask() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.taskA;
+            } else {
+                return this.taskB;
+            }
+        }
+    }
 });
 
 app.mount('#tasks');
